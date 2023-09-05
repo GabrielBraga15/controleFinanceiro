b0a1c63ed7503e05d7e4e9599ff4c3e7dbe0820d
Gabriel Braga <gabrielferreira9034@gmail.com>
2023-09-05 10:31:59 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let saldo =prompt ("Digite aqui a quantidade inicial de dinheiro disponível.")
+saldo = parseFloat(saldo)
+let opcao = ""
+
+
+
+do {
+  opcao = prompt (
+  "Saldo disponivel: R$ " + saldo +
+  "\n1. Adicionar dinheiro" + 
+  "\n2. Remover dinheiro" + 
+  "\n3. Sair" 
+  )
+  switch (opcao) {
+    case "1":
+      saldo += parseFloat (prompt ("Informe o valor a ser adicionado"))
+      break
+      case "2":
+        saldo -= prompt("Informe o valor a ser removido")
+        break
+        case "3":
+          alert ("Saindo...")
+          break
+          default:
+            alert("Entrada inválida")
+  }
+} while (opcao !== "3") 
